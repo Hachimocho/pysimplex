38a35ab30000a6c28d45b44a5b2564b9d85a20ca
brg2890 <brg2890@rit.edu>
2022-10-15 00:19:21 -0400

    Added pivoting, converted to use fractions, updated output formatting, added more input checks, completed main function.

--- simplex.py
+++ simplex.py
@@ -1,7 +1,10 @@
 # Authors: Aidan Lynch, Bryce Gernon, Jim Godin, Ryan Devoe, Sam Ford
 import sys  # Command line argument handling
 import numpy  # Python matrix ops
+from fractions import Fraction
 import os  # Could be useful for saving output to file if desired
+
+import numpy as np
 
 '''
  * Class for storing the result of parsing command line arguments
@@ -10,12 +13,15 @@ import os  # Could be useful for saving output to file if desired
  * m: number of rows
  * n: number of columns
  '''
+
+
 class ArgResult:
 
-	def __init__(self, args : list):
+	def __init__(self):
+		self.m = -1
+		self.n = -1
 		self.success = False
-		self.m = 0
-		self.n = 0
+
 
 '''
  * class for storing the result the payoff matrix entry
@@ -25,23 +31,27 @@ class ArgResult:
  * m: number of rows
  * n: number of columns
  '''
+
+
 class PayoffResult:
 
 	def __init__(self, m, n):  # Equivalent to get_payoff
 		self.success = False
 		self.payoff = []
-		self.m = 0 
-		self.n = 0 
+		self.m = 0
+		self.n = 0
 
 	'''
-	* Sets a PayoffResult object to nothing
-	*
-	* result: payoff result structure
-	'''
+    * Sets a PayoffResult object to nothing
+    *
+    * result: payoff result structure
+    '''
+
 	def free_payoff_result(self):
 		free_2d_arr(self.payoff, self.m)
-		self. n = -1
+		self.n = -1
 		return
+
 
 '''
  * Class to store a pivot result
@@ -51,26 +61,7 @@ class PayoffResult:
  * pivot_row: row of the pivot used
  * pivot_col: col of the pivot used
  '''
-class PivotResult:
 
-	def __init__(self):
-		self.success = False
-		self.tableau = None
-		self.pivot_row = -1
-		self.pivot_col = -1
-
-	'''
-	* Sets a pivot result to nothing
-	*
-	* result: result to be nullified
-	*
-	'''
-	def free_pivot_result(self, result):
-		self.tableau.free_tableau()
-		self.success = False
-		self.pivot_row = None
-		self.pivot_col = None
-		return
 
 '''
  * Struct for storing a tableau
@@ -81,6 +72,8 @@ class PivotResult:
  * rows: number of rows in m
  * cols: number of columns in m
  '''
+
+
 class Tableau:
 	# this is equivalent to create_tableau in Aidan's simplex.c
 	def __init__(self, s_size, x_size):
@@ -91,23 +84,42 @@ class Tableau:
 		self.k = 0
 		# print( self.rows, self.cols, "in tableau constructor")
 		# numpy makes 2 for loops in C into a line of code - SF 10/13
-		self.m = numpy.zeros((self.rows,self.cols))  # Generate m here
+		self.m = numpy.zeros((self.rows, self.cols)).astype('object')  # Generate m here
+		for row, _ in enumerate(self.m):
+			for col, _ in enumerate(self.m[row]):
+				self.m[row][col] = Fraction(0)
 
 	# equivalent to print_tableau in Aidan's simplex.c
 	def __str__(self):
+		# Use numpy's inbuilt array to string conversion
+		#return self.m.__str__()
+
+
+		# Get largest (size) number for formatting purposes
+		max_num_size = 0
+		# for row in range(self.rows):
+		# 	for col in range(self.cols):
+		# 		# Account for size of negative sign
+		# 		if self.m[row][col] < 0:
+		# 			if self.m[row][col]
 		tableau_str = ""
 		for row in range(self.rows):
+			if row == self.s_size:
+				tableau_str += "_____" * self.cols + "\n"
 			for col in range(self.cols):
-				tableau_str += f"{self.m[row][col]} "
+				if col == self.x_size or col == self.x_size + self.s_size:
+					tableau_str += " | "
+				tableau_str += str(Fraction(self.m[row][col]).numerator) + "/" + str(Fraction(self.m[row][col]).denominator) + " "
 			tableau_str += "\n"
 		return tableau_str
 
 	'''
-	* Sets tableau object as non-usable
-	*
-	* tableau: struct to free
-	* 
-	'''
+    * Sets tableau object as non-usable
+    *
+    * tableau: struct to free
+    * 
+    '''
+
 	def free_tableau(self):
 		free_2d_arr(self.m, self.rows)
 		self.s_size = -1
@@ -118,18 +130,24 @@ class Tableau:
 
 
 ''''set the 2d arr to nothing'''
+
+
 def free_2d_arr(arr, rows):
 	arr = None
 	rows = -1
 	return
 
+
 '''
  * Print the usage statement for this program.
  '''
+
+
 def print_usage():
 	print("usage: simplex m n\n")
 	print("\tm: number of rows, integer greater than 0\n")
 	print("\tn: number of columns, integer greater than 0\n")
+
 
 '''
  * Parses this program's command line arguments.
@@ -139,21 +157,25 @@ def print_usage():
  *
  * return: arg result structure
  '''
-def parse_args( argc : int, argv : list) -> ArgResult:
-	result = ArgResult(argv)
+
+
+def parse_args(argc: int, argv: list) -> ArgResult:
+	result = ArgResult()
 
 	# do we want to prompt for input if
 	# rows and cols not provided? - SF 10/13
-	if(argc != 3):
+	if (argc != 3):
 		result.m = -1
 		result.n = -1
 		return result
-	
-    # parse row and column numbers
+
+	# parse row and column numbers
 	try:
-		m : int = int(argv[1])
-		n : int = int(argv[2])
-	except ValueError:
+		m: int = int(argv[1])
+		assert m > 0
+		n: int = int(argv[2])
+		assert n > 0
+	except Exception:
 		result.m = -1
 		result.n = -1
 		return result
@@ -162,6 +184,7 @@ def parse_args( argc : int, argv : list) -> ArgResult:
 	result.n = n
 	result.success = True
 	return result
+
 
 '''
  * Prompt user for payoff matrix and process its entry.
@@ -171,23 +194,25 @@ def parse_args( argc : int, argv : list) -> ArgResult:
  *
  * return: payoff result structure
  '''
-def get_payoff (m : int, n : int) -> PayoffResult:
-	result = PayoffResult(m,n)
+
+
+def get_payoff(m: int, n: int) -> PayoffResult:
+	result = PayoffResult(m, n)
 	# initialize a mxn numpy array
-	result.payoff = numpy.zeros((m,n))
+	result.payoff = numpy.zeros((m, n))
 	result.m = m
 	result.n = n
 
-	#string interpolation
+	# string interpolation
 	print(f"Please enter the {m} by {n} payoff matrix below."
-	"\nSeparate rows by new lines and columns by spaces: \n")
-	
-	mat_str : str = ""
-	row_els : list = []
-	r_count : int = -1
+	      "\nSeparate rows by new lines and columns by spaces: \n")
+
+	mat_str: str = ""
+	row_els: list = []
+	r_count: int = -1
 	lineNum = 0
 	while lineNum < m:
-		mat_str = input() 
+		mat_str = input()
 		row_els = mat_str.split()
 		r_count = len(row_els)
 
@@ -195,22 +220,23 @@ def get_payoff (m : int, n : int) -> PayoffResult:
 			print(f"Expected {n} entries in a row. Got {r_count} Aborting.")
 			result.success = False
 			return result
-		
+
 		# validate the input we got was numbers
 		for col in range(0, n):
 			try:
-				num : float = float(row_els[col])
-				result.payoff[lineNum][col]  = num
+				num = Fraction(row_els[col])
+				result.payoff[lineNum][col] = num
 			except ValueError:
-				print("Couldn't convert input string to floats")
+				print("Couldn't convert input string to fraction")
 				result.success = False
 				free_2d_arr(result.payoff, m)
 				return result
-		#endfor
-		lineNum +=1
-	#endwhile
+		# endfor
+		lineNum += 1
+	# endwhile
 	result.success = True
 	return result
+
 
 '''
  * Builds the initial tableau using the given payoff matrix
@@ -221,46 +247,66 @@ def get_payoff (m : int, n : int) -> PayoffResult:
  *
  * return: initial tableau
  '''
+
+
 def get_init_tableau(payoff, m, n) -> Tableau:
-	tableau : Tableau = Tableau(m,n)
-	
-	min : float = sys.maxsize 
+	tableau: Tableau = Tableau(m, n)
+
+	min = sys.maxsize
 	for row in range(0, m):
 		for col in range(0, n):
-			if(payoff[row][col] < min):
+			if (payoff[row][col] < min):
 				min = payoff[row][col]
-	
-	# Python ternary assignment of booster val k
-	k : float = 1 - min if (min < 1) else 0
 
-	# populate the initial tableau so that every entry >= 
+	# Python ternary assignment of booster val k
+	k = Fraction(1 - min) if (min < 1) else Fraction(0)
+
+	# populate the initial tableau so that every entry >=
 	# in the elif the == will eval to 1 or 0 since
 	# True or False is equiv to those respectively
 	for row in range(0, m):
 		for col in range(0, n):
-			if( col < n ):
-				tableau.m[row][col] = payoff[row][col] + k \
-				if (row < m) else -1
-			elif(col < n + m): 
-				tableau.m[row][col] = (float) (row == col - n) \
-				if (row < m) else 0
+			if (col < n):
+				tableau.m[row][col] = Fraction(payoff[row][col] + k) \
+					if (row < m) else Fraction(-1)
+			elif (col < n + m):
+				tableau.m[row][col] = Fraction((row == col - n)) \
+					if (row < m) else Fraction(0)
 			else:
-				tableau.m[row][col] = (float) (row < m)
+				tableau.m[row][col] = Fraction((row < m))
 
 	return tableau
+
 
 '''
  * Pivots the provided tableau
  *
  * tableau: struct to pivot
  '''
-def pivot_tableau(tableau : Tableau) -> PivotResult:
-	result : PivotResult = PivotResult()
-	result.tableau = Tableau(tableau.s_size, tableau.x_size)
-	result.tableau = tableau.k
 
-	#rest for me later on or whoever works next.
-	return result
+
+def pivot_tableau(tableau: Tableau) -> Tableau:
+	pivot_col = np.argmin(tableau.m[tableau.rows-1])
+	min_value = sys.maxsize
+	pivot_row = Fraction(-1)
+	for row, entry in enumerate(np.swapaxes(tableau.m, 0, 1)[pivot_col][0:-1]):
+		value = Fraction(1 / entry)
+		if 0 < value < min_value:
+			min_value = value
+			pivot_row = row
+	pivot_value = Fraction(tableau.m[pivot_row][pivot_col])
+	prev_tableau = np.copy(tableau.m)
+	for col, _ in enumerate(tableau.m[pivot_row]):
+		tableau.m[pivot_row][col] = Fraction(tableau.m[pivot_row][col] / pivot_value)
+
+	for row, _ in enumerate(tableau.m):
+		for col, _ in enumerate(tableau.m[row]):
+			if row != pivot_row:
+				tableau.m[row][col] = Fraction(tableau.m[row][col] - Fraction(prev_tableau[row][pivot_col] * tableau.m[pivot_row][col]))
+
+	return tableau
+
+
 '''
  * Runs the simplex method on the supplied payoff matrix.
  *
@@ -269,48 +315,45 @@ def pivot_tableau(tableau : Tableau) -> PivotResult:
  *
  * return: 0 on successful execution
  '''
+
+
 def main():
-	for arg in sys.argv:
-		print(arg)
+	# Parse inputs
+	parse_result: ArgResult = parse_args(len(sys.argv), sys.argv)
 
-	parse_result : ArgResult = parse_args(len(sys.argv),sys.argv)
-	
-	if(parse_result.success == False):
+	if not parse_result.success:
 		return -1
-
+	# Get initial matrix
 	payoff_result = get_payoff(parse_result.m, parse_result.n)
-	
-	if(payoff_result.success == False):
+
+	if not payoff_result.success:
 		return -1
 
-	m : int = payoff_result.m
-	n : int = payoff_result.n
-	print( m, n)
-	# set order of x variables to be -1 for the m rows
-	order : list = [-1] * m 
-	tableau : Tableau = get_init_tableau(payoff_result.payoff, m, n)
+	m: int = payoff_result.m
+	n: int = payoff_result.n
+
+	tableau: Tableau = get_init_tableau(payoff_result.payoff, m, n)
+	# Add default values to tableau
+	for x in range(m):
+		tableau.m[n][x] = -1
+		tableau.m[x][m+n] = 1
+		tableau.m[x][n+x] = 1
 	print("Initial Tableau:")
 	print(tableau)
-	
+
 	pivot_count = 0
 
 	# while true hype...incomplete SF 10/13
-	while(True):
-		pivot_result : PivotResult = pivot_tableau(tableau)
-		
-		#placeholder for no perma-loop
-		if(True == True):
+	while True:
+		tableau = pivot_tableau(tableau)
+		pivot_count += 1
+		print("Pivot #" + str(pivot_count) + ":")
+		# break when no negative values on bottom row
+		if np.min(tableau.m[tableau.rows-1]) >= 0:
 			break
-		#do something maybe...
-		if(pivot_result.success == True):
-			break
-		#get outta the loop
-		else: 
-			break
-		
-		pviot_count += 1
 
 	return 0
+
 
 if __name__ == '__main__':
 	main()
